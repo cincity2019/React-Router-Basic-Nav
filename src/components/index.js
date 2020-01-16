99e8d225336a8dca02c9d297ba240e15f01373b7
cincity2019 <cnkelly73@gmail.com>
2020-01-15 19:47:17 -0500

    psuh1

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,17 @@
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Navigation from './Navigation';
+// import Home from './Home';
+// import About from './About';
+// import Contact from './Contact';
+// import Navigation from './Navigation';
 
-export { Home, About, Contact, Navigation };
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// export { Home, About, Contact, Navigation };
+
+// const rootElement = document.getElementById('root');
+
+// ReactDOM.render(
+// 	<Router>
+// 		<App />
+// 	</Router>,
+// 	document.getElementById('root').rootElement
+// );
